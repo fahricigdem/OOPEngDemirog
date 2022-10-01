4d599072715fb1a850d51f5a635c91373791662b
fahricigdem <fahricigdem34@gmail.com>
2022-10-01 23:37:24 +0200

    methods

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -7,7 +7,7 @@ using Inheritance;
 
 Customer customer=new Customer();
 customer.FirstName = "Ali";
-Console.WriteLine(customer.FirstName);
+//Console.WriteLine(customer.FirstName);
 
 Person[] people = new Person[] {
     new Person{ FirstName="Engin" },
@@ -17,5 +17,27 @@ Person[] people = new Person[] {
 
 foreach (Person person in people)
 {
+    //Console.WriteLine(person.FirstName);
+}
+
+List<Person> people2 = new List<Person>();
+Student student1 = new Student { Id = 1, FirstName = "Onur", LastName = "Cigdem", Department = "Computer" };
+Customer customer1 = new Customer { Id = 1, FirstName = "Onur", LastName = "Cigdem",City="Tokat" };
+Ekle(customer1);
+Ekle(student1);
+Sil(customer1);
+void Ekle(Person person)
+{
+    people2.Add(person);
+}
+void Sil(Person person)
+{
+    people2.Remove(person);
+}
+
+
+foreach (var person in people2)
+{
     Console.WriteLine(person.FirstName);
+    if (person is Student) Console.WriteLine(((Student)person).Department);
 }
